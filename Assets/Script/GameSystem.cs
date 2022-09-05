1e45151acb3e8dd1afd1ecd9a40251cd36d43c4e
hidenobu-obata <99260242+hidenobu-obata@users.noreply.github.com>
2022-09-05 18:56:30 +0900

    repair

--- Assets/Script/GameSystem.cs
+++ Assets/Script/GameSystem.cs
@@ -2,18 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour
 {
+
+[SerializeField] Text timerText = default;
+int timeCount;
+
+[SerializeField] GameObject resultPanel = default;
+
+bool gameOver;
+
     void Start()
     {
+        timeCount = 90;
         SoundManager.instance.PlayBGM(SoundManager.BGM.Main);
+        StartCoroutine(CountDown());
+        
     }
 
-    public void Retry()
+   IEnumerator CountDown()
     {
-        string thisScene = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(thisScene);
+        while (timeCount > 0)
+        {
+
+        yield return new WaitForSeconds(1);
+        timeCount--;
+        timerText.text = timeCount.ToString();
+        }
+        Debug.Log("timeup");
+        gameOver = true;
+
+        resultPanel.SetActive(true);
+
     }
+
+    public void OnRetryButton()
+
+{
+    SceneManager.LoadScene("Main");
+
+}
+
+void Update()
+    {
+    if(gameOver)
+    {
+    return;
+
+    }
+
+
+    }
+
 }
